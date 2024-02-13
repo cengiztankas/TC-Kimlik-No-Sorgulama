2468c1d6f1f33c79d882ef3a6074786e04cc0a7b
Cengiz Tankaş <cengizasd.3810@gmail.com>
2024-02-13 22:25:55 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TcKimlikNoSorgulama_Soap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsorgula = new System.Windows.Forms.Button();
+            this.txttc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtdogumyili = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnsorgula
+            // 
+            this.btnsorgula.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsorgula.Location = new System.Drawing.Point(128, 223);
+            this.btnsorgula.Name = "btnsorgula";
+            this.btnsorgula.Size = new System.Drawing.Size(196, 33);
+            this.btnsorgula.TabIndex = 0;
+            this.btnsorgula.Text = "Kişiyi Sorgula";
+            this.btnsorgula.UseVisualStyleBackColor = true;
+            this.btnsorgula.Click += new System.EventHandler(this.btnsorgula_Click);
+            // 
+            // txttc
+            // 
+            this.txttc.Location = new System.Drawing.Point(126, 60);
+            this.txttc.Name = "txttc";
+            this.txttc.Size = new System.Drawing.Size(196, 22);
+            this.txttc.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(37, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TC No:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnsorgula);
+            this.panel1.Controls.Add(this.txtdogumyili);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtsoyad);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtad);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txttc);
+            this.panel1.Location = new System.Drawing.Point(67, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 296);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(67, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ad:";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(126, 103);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(196, 22);
+            this.txtad.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(39, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Soyad:";
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(128, 144);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(196, 22);
+            this.txtsoyad.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(5, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Doğum Yılı:";
+            // 
+            // txtdogumyili
+            // 
+            this.txtdogumyili.Location = new System.Drawing.Point(128, 185);
+            this.txtdogumyili.Name = "txtdogumyili";
+            this.txtdogumyili.Size = new System.Drawing.Size(196, 22);
+            this.txtdogumyili.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(68, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "TC Kimlik Sorgulama";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(496, 455);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TC Kimlik Sorgulama";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsorgula;
+        private System.Windows.Forms.TextBox txttc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtdogumyili;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcKimlikNoSorgulama_Soap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsorgula_Click(object sender, EventArgs e)
+        {
+            long tc=long.Parse(txttc.Text);
+            int yil = int.Parse(txtdogumyili.Text);
+            bool durum = false;
+            try
+            {
+                using(TcKimlikDogrulama.KPSPublicSoapClient tcService = new TcKimlikDogrulama.KPSPublicSoapClient { })
+                {
+                  durum=  tcService.TCKimlikNoDogrula(tc, txtad.Text, txtsoyad.Text, yil);
+                   
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            if(durum)
+            {
+                MessageBox.Show("Bu Kullanıcı Sistemde Kayıtlı");
+            }
+            else
+            {
+                MessageBox.Show("!!! Bu Kullanıcı Sistemde Kayıtlı Değil");
+
+            }
+        }
+    }
+}
